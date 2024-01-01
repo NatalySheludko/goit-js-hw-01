fde138e97aa371a81baa7da99eca1517eb539d28
NatalySheludko <137063086+NatalySheludko@users.noreply.github.com>
2024-01-01 13:47:27 +0200

    completed tasks.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,14 @@
-"use strict";
+'use strict'
 
 function getElementWidth(content, padding, border) {
-  let contentNpx = parseFloat(content);
-  let paddingNpx = parseFloat(padding);
-  let borderNpx = parseFloat(border);
-  let Npx = contentNpx + 2 * paddingNpx + 2 * borderNpx;
-  return Npx;
+  let contentNpx = parseFloat(content)
+  let paddingNpx = parseFloat(padding)
+  let borderNpx = parseFloat(border)
+  let Npx = contentNpx + paddingNpx * 2 + borderNpx * 2
+  return Npx
 
   //return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-
-  //return (
-  //parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border)
-  //);
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth('50px', '8px', '4px')) // 74
+console.log(getElementWidth('60px', '12px', '8.5px')) // 101
+console.log(getElementWidth('200px', '0px', '0px')) // 200
